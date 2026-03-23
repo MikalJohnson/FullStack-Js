336442d773a51274d9471f395b956d52bcdfa5ef
KaleTech <95278000+MikalJohnson@users.noreply.github.com>
2026-03-23 15:17:01 -0700

    Created objects and arrays for my facebook replica app

---
+++ javascript/objects/object.js
@@ -0,0 +1,30 @@
+// Create an object and an array which we will use in our facebook exercise.
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+let info = {
+    username: "MikalJohnson",
+    password: "Cougars24",
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+let database = [
+    {
+        username: info.username,
+        password: info.password
+    }
+];
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+let newsfeed = [
+    {
+        username: "Henry",
+        timeline: "I just obtain my first job!!!"
+    },
+    {
+        username: "Lewis",
+        timeline: "Going hiking this weekend, who wants to join?"
+    },
+    {
+        username: "Katy",
+        timeline: "I going to Italy soon"
+    }
+];
